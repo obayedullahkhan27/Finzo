a2e466c870d81c043d76c63564d2089c47b89d46
Tarikul Obayedullah Khan <ullahtarik300@gmail.com>
2026-03-31 00:02:08 +0600

    project setup completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
+import "@/global.css";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+      <Link
+        href={"/onboarding"}
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Onboarding
+      </Link>
     </View>
   );
 }
